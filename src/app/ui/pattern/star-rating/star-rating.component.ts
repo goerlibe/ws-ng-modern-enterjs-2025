4c5081b0877895835c72e41a13c5712191bc7bb8
Bertin Goerlich <bertinnico.goerlich@dkfz-heidelberg.de>
2025-05-06 12:45:41 +0200

    refactor: use a signal and a computed signal for the star rating

--- src/app/ui/pattern/star-rating/star-rating.component.ts
+++ src/app/ui/pattern/star-rating/star-rating.component.ts
@@ -1,9 +1,14 @@
 import { NgClass, NgFor, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  Input,
+  signal,
+} from '@angular/core';
 
 const range = 10;
 const numStars = 5;
-const starsArray: number[] = new Array(numStars).fill(1);
 
 @Component({
   selector: 'ui-star-rating',
@@ -13,7 +18,7 @@ const starsArray: number[] = new Array(numStars).fill(1);
     </span>
     <div class="stars">
       <span
-        *ngFor="let fill of stars"
+        *ngFor="let fill of stars()"
         class="star"
         [ngClass]="{
           'star-half': fill === 0,
@@ -35,28 +40,27 @@ const starsArray: number[] = new Array(numStars).fill(1);
 export class StarRatingComponent {
   range = range;
   numStars = numStars;
-  stars: number[] = starsArray;
-  @Input() showRating = false;
-  tooltipText = `0 average rating`;
-
-  private _rating = 5;
-  @Input()
-  set rating(rating: number | undefined) {
-    this._rating = rating || 0;
-
-    this.setToolTopText(this.rating);
-
-    const scaledRating = this._rating / (this.range / this.numStars);
+  stars = computed(() => {
+    const scaledRating = this._rating() / (this.range / this.numStars);
     const full = Math.floor(scaledRating);
     const half = scaledRating % 1 > 0.5 ? 1 : 0;
     const empty = this.numStars - full - half;
-    this.stars = new Array(full)
+    return new Array(full)
       .fill(1)
       .concat(new Array(half).fill(0))
       .concat(new Array(empty).fill(-1));
+  });
+  @Input() showRating = false;
+  tooltipText = `0 average rating`;
+
+  private _rating = signal(5);
+  @Input()
+  set rating(rating: number | undefined) {
+    this._rating.set(rating || 0);
+    this.setToolTopText(this.rating);
   }
   get rating(): number {
-    return this._rating;
+    return this._rating();
   }
 
   private setToolTopText(rating: number) {
